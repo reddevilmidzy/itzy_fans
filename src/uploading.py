e619c188b0369b11cb00542191bbfcdcb683f851
은지 <midzy0228@gmail.com>
2022-12-30 20:15:43 +0900

    feat: 텍스트 포멧 메소드 분리

--- src/uploading.py
+++ src/uploading.py
@@ -1,14 +1,15 @@
 import tweepy
 
-def update_tweet(title, link, community):
-    api_key = ""
-    api_key_secret = ""
-    access_token = ""
-    access_token_secret = ""
+api_key = ""
+api_key_secret = ""
+access_token = ""
+access_token_secret = ""
 
+def update_tweet(title, link, community):
     auth = tweepy.OAuth1UserHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    text = f"ITZY Fan's\n[{community}] 새 글 알림\n\n{title}\n\n{link}\n\n#ITZY #있지"
+    api.update_status(formatted_string(title, link, community))
 
-    api.update_status(text)
+def formatted_string(title, link, community):
+    return f"ITZY Fan's\n[{community}] 새 글 알림\n\n{title}\n\n{link}\n\n#ITZY #있지"
